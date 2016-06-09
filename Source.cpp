dc9cfac4f5ee7540b00009d49ddcf1f17a7f5947
TerezaCM13 <princesadigital01@gmail.com>
2016-06-09 10:55:17 -0300

    HelloWord
    Olámundo

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+
+//Incluindo a biblioteca iostream,que me dá "superpoderes".
+#include <iostream>
+
+using namespace std;
+int main() main=código, todo meu código é o main,ou seja,é a função principal.Nesta linha estou afirmando que, minha função main tem o tipo inteiro.
+{ //Começando o meu código.
+
+	cout << "Hello World" << endl; // cout=minha entrada. Estou mostrando oque eu quero imprimir na tela, e estou finalizando com o meu endl=saída.
+
+
+	return 0; //Retornando ao começo, ou seja voltando a zero(o).
+
+
+} //Fechando meu código, ou seja finalizando.
